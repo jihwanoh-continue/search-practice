d6a3b2ed0b3d5408363374582a3e891b538800b6
jihwanoh-continue <jihwan9872@gmail.com>
2024-10-16 00:50:32 +0900

    feat: keyword list를 조회할 수 있는 기능 구현

--- src/main/kotlin/jihwan/practice/search/controller/search/SearchController.kt
+++ src/main/kotlin/jihwan/practice/search/controller/search/SearchController.kt
@@ -1,6 +1,7 @@
 package jihwan.practice.search.controller.search
 
 import jihwan.practice.search.service.SearchService
+import jihwan.practice.search.service.dto.Keyword
 import jihwan.practice.search.service.dto.Place
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,5 +16,10 @@ class SearchController(
     @GetMapping
     override suspend fun search(@RequestParam keyword: String): List<Place> {
         return searchService.search(keyword)
+    }
+
+    @GetMapping("/keywords")
+    override fun getTopKeywords(size: Int): List<Keyword> {
+        return searchService.getTopKeywords(size)
     }
 }
