b2723eaf69ff61b4bebce15a279e5a1c9bb7e3aa
jihwanoh-continue <jihwan9872@gmail.com>
2024-10-16 22:00:52 +0900

    feat: 문서화 및 swagger 구현 - readme 작성 - swagger 구현 - 테스트할 수 있는 방법 작성

--- src/main/kotlin/jihwan/practice/search/client/NaverPlaceSearchClient.kt
+++ src/main/kotlin/jihwan/practice/search/client/NaverPlaceSearchClient.kt
@@ -2,6 +2,7 @@ package jihwan.practice.search.client
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import jihwan.practice.search.configuration.client.ApiClient
+import jihwan.practice.search.util.log.logger
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 import org.springframework.web.reactive.function.client.WebClientResponseException
@@ -12,6 +13,8 @@ class NaverPlaceSearchClient(
     companion object {
         private const val FORMAT = "json"
     }
+
+    private val logger = logger<NaverPlaceSearchResponse>()
 
     fun search(keyword: String, size: Int): NaverPlaceSearchResponse? {
         val params: MultiValueMap<String, String> = LinkedMultiValueMap()
@@ -26,7 +29,7 @@ class NaverPlaceSearchClient(
             )
         } catch (ex: WebClientResponseException) {
             if (ex.statusCode.is5xxServerError) {
-                println("Naver API 서버에서 에러가 발생했습니다.")
+                logger.error("Naver Place Search API 서버 오류", ex)
             }
             null
         }
